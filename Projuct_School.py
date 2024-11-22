44ec75c025a3d1046904fb53e7cbd1f048ab9ba2
AbdAlkarim Yusef Nairoukh <22310231@scme.edu.ps>
2024-11-22 06:51:28 +0200

    Update Projuct_School.py

--- Projuct_School.py
+++ Projuct_School.py
@@ -69,6 +69,32 @@ def attendance_menu():
             break
         else:
             print("Invalid choice. Please try again.")
+
+# QuizSystem ...> نظام الامتحانات
+def QuizSystem():
+    # Students' answers to the questions
+    answers = [
+      ['A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'],
+      ['D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'],
+      ['E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'],
+      ['C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'],
+      ['A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'],
+      ['B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'],
+      ['B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'],
+      ['E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D']]
+      
+    # Key to the questions
+    keys = ['D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D']
+    
+    # Grade all answers
+    for i in range(len(answers)):
+        # Grade one student
+        correctCount = 0
+        for j in range(len(answers[i])): 
+            if answers[i][j] == keys[j]:
+                correctCount += 1
+
+        print("Student", i, "'s correct count is", correctCount,"/10")
     
 # Input information Student
 class student:
@@ -217,7 +243,8 @@ def main():
     print("\nPlease choose your section:","\n")
     print("1. certificate")
     print("2. School cafeteria")
-    print("3. Attendance System","\n", "*" * 30)
+    print("3. Attendance System")
+    print("4. Quiz System","\n", "*" * 30)
     section_choice = int(input("Please Enter the number: "))
     print("*" * 50)
 
@@ -253,6 +280,8 @@ def main():
         x.print_info()
     elif section_choice == 3:
         attendance_menu()
+    elif section_choice == 4:
+        QuizSystem()
     else:
         print("\nPlease try again","\n")
 
